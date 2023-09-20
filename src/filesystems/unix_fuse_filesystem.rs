f68b3e624a8bd355b8f8e256f30aa580df191631
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2023-09-19 23:50:50 -0700

    support debugging on unix

--- src/filesystems/unix_fuse_filesystem.rs
+++ src/filesystems/unix_fuse_filesystem.rs
@@ -1,30 +1,55 @@
 use crate::filesystems::FuseFileSystem;
-use crate::synology_api::FileStation;
+use crate::synology_api::FileStationFileSystem;
 
 use std::ffi::OsStr;
 use fuse::Filesystem;
 
 struct UnixFileSystemHandler {
+    filestation_filesystem: FileStationFileSystem
+}
 
+impl UnixFileSystemHandler {
+    pub fn new(filestation_filesystem: FileStationFileSystem) -> UnixFileSystemHandler {
+        UnixFileSystemHandler {
+            filestation_filesystem: filestation_filesystem
+        }
+    }
 }
 
 impl Filesystem for UnixFileSystemHandler {
 }
 
 pub struct UnixFuseFileSystem {
+    hostname: String,
+    port: u16,
+    secured: bool,
+    version: u8,
 }
 
 impl FuseFileSystem for UnixFuseFileSystem {
-    fn new(hostname: &str, port: u16, secured: bool, version: u8, debug: bool) -> UnixFuseFileSystem {
-        UnixFuseFileSystem {}
+    fn new(hostname: &str, port: u16, secured: bool, version: u8, _debug: bool) -> UnixFuseFileSystem {
+        UnixFuseFileSystem {
+            hostname: hostname.to_string(),
+            port,
+            secured,
+            version,
+        }
     }
 
     fn mount(&mut self, mount_point: &str, username: &str, password: &str) {
+        let mut filestation_filesystem = FileStationFileSystem::new(
+            &self.hostname,
+            self.port,
+            self.secured,
+            self.version
+        );
+        filestation_filesystem.login(username, password).unwrap();
+
         let options = ["-o", "ro", "-o", "fsname=SYNO_FileStation"]
             .iter()
             .map(|o| o.as_ref())
             .collect::<Vec<&OsStr>>();
-        fuse::mount(UnixFileSystemHandler {}, &OsStr::new(mount_point), &options).unwrap();
+        fuse::mount(UnixFileSystemHandler::new(filestation_filesystem), &OsStr::new(mount_point), &options).unwrap();
     }
 
     fn unmount(&self) {
